d03e2b616e0e56cedd96257151edde678610cf19
Selvin-rko <98406215+Selvin-rko@users.noreply.github.com>
2022-02-04 22:06:09 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../clevertap_sw.js')
+      .register('Scripts/clevertap_sw.js')
       .then(reg => console.log('Service Worker: Registered (Pages)'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
