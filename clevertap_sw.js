0e0c5c970d199c443a2afccdaafe9909fded5f49
Selvin-rko <98406215+Selvin-rko@users.noreply.github.com>
2023-03-15 17:31:52 +0530

    Update clevertap_sw.js

--- clevertap_sw.js
+++ clevertap_sw.js
@@ -1,12 +1,3 @@
 importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js');
 
-   if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('~clevertap_sw.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      //registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  }else {
-    console.log('No service-worker on this browser');
-  }
+ 
